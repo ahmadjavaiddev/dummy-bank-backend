c7be5ba90d45f660bbb55cfbf440b9384c486c23
ahmadjavaiddev <ahmadjavaiddev@gmail.com>
2024-06-22 18:53:03 +0500

    add constant file to store all constants

---
+++ src/constants.js
@@ -0,0 +1,4 @@
+export const UserSelectSecureSchema =
+    "-password -accessToken -accessTokenId -accessTokenExpiry -mPin -verifiedIPS -haveCard -balance -virtualCard -__v";
+export const UserSelectSchema =
+    "-password -accessToken -accessTokenId -accessTokenExpiry -mPin -verifiedIPS -haveCard -balance -virtualCard -__v";
