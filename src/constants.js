6d0c291e7dac7d906895eb3e8e6dd0b2ad8899cf
ahmadjavaiddev <ahmadjavaiddev@gmail.com>
2024-06-22 22:47:34 +0500

    Move the cookieOptions to constants file

--- src/constants.js
+++ src/constants.js
@@ -2,3 +2,8 @@ export const UserSelectSecureSchema =
     "-password -accessToken -accessTokenId -accessTokenExpiry -mPin -verifiedIPS -haveCard -balance -virtualCard -__v";
 export const UserSelectSchema =
     "-password -accessToken -accessTokenId -accessTokenExpiry -mPin -verifiedIPS -haveCard -balance -virtualCard -__v";
+
+export const cookieOptions = {
+    httpOnly: true,
+    secure: true,
+};
