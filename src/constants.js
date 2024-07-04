0d39037a891ea7123020fd1c177a3c0d5922477e
ahmadjavaiddev <ahmadjavaiddev@gmail.com>
2024-07-05 01:16:55 +0500

    Add Day and time constants

--- src/constants.js
+++ src/constants.js
@@ -8,3 +8,11 @@ export const cookieOptions = {
     secure: true,
     sameSite: "None",
 };
+
+// HOURS OR TIME OR DAY
+export const oneHour = new Date(Date.now() + 1 * 1000 * 60 * 60);
+export const fifteenMinutes = new Date(Date.now() + 1 * 1000 * 60 * 15);
+export const fiveDays = new Date(Date.now() + 1 * 1000 * 60 * 60 * 24 * 5);
+
+// VERIFICATION CODES
+export const sixDigit = Math.floor(100000 + Math.random() * 900000);
