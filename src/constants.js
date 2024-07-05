46589cb331d320a858f9d86cb49021cae60ea411
ahmadjavaiddev <ahmadjavaiddev@gmail.com>
2024-07-05 23:21:14 +0500

    Update Functions

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,9 @@
 export const UserSelectSecureSchema =
-    "-password -accessToken -accessTokenId -accessTokenExpiry -mPin -verifiedIPS -haveCard -balance -virtualCard -ipVerificationCode -__v";
+    "-password -accessToken -accessTokenId -accessTokenExpiry -mPin -verifiedIPS -haveCard -balance -virtualCard -ipVerificationCode -lastLoginIP -ipVerifyEmail -__v";
 export const UserSelectSchema =
     "-password -accessToken -accessTokenId -accessTokenExpiry -mPin -verifiedIPS -haveCard -balance -virtualCard -__v";
+export const UserSelectWithIP =
+    "-password -accessToken -accessTokenId -accessTokenExpiry -mPin -verifiedIPS -haveCard -virtualCard -__v";
 
 export const cookieOptions = {
     httpOnly: true,
@@ -10,9 +12,22 @@ export const cookieOptions = {
 };
 
 // HOURS OR TIME OR DAY
-export const oneHour = new Date(Date.now() + 1 * 1000 * 60 * 60);
-export const fifteenMinutes = new Date(Date.now() + 1 * 1000 * 60 * 15);
-export const fiveDays = new Date(Date.now() + 1 * 1000 * 60 * 60 * 24 * 5);
+export const oneHour = () => {
+    const numbers = new Date(Date.now() + 1 * 1000 * 60 * 60);
+    return numbers;
+};
+export const fifteenMinutes = () => {
+    const number = new Date(Date.now() + 1 * 1000 * 60 * 15);
+    return number;
+};
+// export const fiveDays = new Date(Date.now() + 1 * 1000 * 60 * 60 * 24 * 5);
+export const fiveDays = () => {
+    const numbers = new Date(Date.now() + 1 * 1000 * 60 * 60 * 24 * 5);
+    return numbers;
+};
 
 // VERIFICATION CODES
-export const sixDigit = Math.floor(100000 + Math.random() * 900000);
+export const sixDigit = () => {
+    const code = Math.floor(100000 + Math.random() * 900000);
+    return code;
+};
