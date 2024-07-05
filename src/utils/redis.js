311af5030eb6a77fd9417d8c54f2e8f1200251f7
ahmadjavaiddev <ahmadjavaiddev@gmail.com>
2024-07-05 23:21:55 +0500

    change redis from aiven to app.redis

--- src/utils/redis.js
+++ src/utils/redis.js
@@ -1,6 +1,12 @@
 import Redis from "ioredis";
 
-const redisClient = new Redis(process.env.REDIS_URI, {
+// const redisClient = new Redis(process.env.REDIS_URI, {
+//     maxRetriesPerRequest: null,
+// });
+const redisClient = new Redis({
+    host: process.env.APP_REDIS_HOST,
+    port: process.env.APP_REDIS_PORT,
+    password: process.env.APP_REDIS_PASSWORD,
     maxRetriesPerRequest: null,
 });
 
