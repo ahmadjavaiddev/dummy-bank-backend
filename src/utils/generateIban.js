a3761436cd391908f093155dae677c2bab104a83
ahmadjavaiddev <ahmadjavaiddev@gmail.com>
2024-07-11 13:44:16 +0500

    Add util to generate unique IBAN and account number

--- src/utils/generateIban.js
+++ src/utils/generateIban.js
@@ -1,26 +1,5 @@
 import User from "../models/user.model.js";
 
-function generateIBAN(countryCode, bankCode, accountNumber) {
-    if (!/^[A-Z]{2}$/.test(countryCode)) {
-        throw new Error("Invalid country code");
-    }
-    if (!/^\d+$/.test(bankCode) || !/^\d+$/.test(accountNumber)) {
-        throw new Error("Bank code and account number should be numeric");
-    }
-
-    const bban = bankCode + accountNumber;
-    const rearranged = bban + countryCode + "00";
-    const converted = rearranged.replace(/[A-Z]/g, (char) =>
-        (char.charCodeAt(0) - 55).toString()
-    );
-
-    const checksum = 98n - (BigInt(converted) % 97n);
-    const checkDigits = checksum.toString().padStart(2, "0");
-    const iban = countryCode + checkDigits + bban;
-
-    return iban;
-}
-
 async function generateUniqueAccountNumber() {
     let accountNumber;
     let exists;
@@ -35,4 +14,27 @@ async function generateUniqueAccountNumber() {
     return accountNumber;
 }
 
-export { generateIBAN, generateUniqueAccountNumber };
+async function generateIBAN() {
+    if (!/^[A-Z]{2}$/.test("DF")) {
+        throw new Error("Invalid country code");
+    }
+
+    const accountNumber = await generateUniqueAccountNumber();
+    if (!/^\d+$/.test("10000000") || !/^\d+$/.test(accountNumber)) {
+        throw new Error("Bank code and account number should be numeric");
+    }
+
+    const bban = "10000000" + accountNumber;
+    const rearranged = bban + "DF" + "00";
+    const converted = rearranged.replace(/[A-Z]/g, (char) =>
+        (char.charCodeAt(0) - 55).toString()
+    );
+
+    const checksum = 98n - (BigInt(converted) % 97n);
+    const checkDigits = checksum.toString().padStart(2, "0");
+    const IBAN = "DF" + checkDigits + bban;
+
+    return { IBAN, accountNumber };
+}
+
+export { generateIBAN };
