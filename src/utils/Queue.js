ac57efb9c5d6bf891b88df286b5db86a2dc693a7
ahmadjavaiddev <ahmadjavaiddev@gmail.com>
2024-07-11 13:45:25 +0500

    Add custom queues

--- src/utils/Queue.js
+++ src/utils/Queue.js
@@ -1,8 +1,41 @@
-import { Queue } from "bullmq";
 import { redisClient } from "./redis.js";
 
-const emailQueue = new Queue("emailQueue", {
-    connection: redisClient,
-});
+const emailQueue = async (
+    name,
+    { userName, email, type, subject, verificationCode }
+) => {
+    try {
+        const data = {
+            userName: userName,
+            email: email,
+            type: type,
+            subject: subject,
+            verificationCode: verificationCode,
+        };
+        const jobId = await redisClient.rpush(
+            "queue:e:emails",
+            JSON.stringify(data)
+        );
 
-export { emailQueue };
+        return jobId;
+    } catch (error) {
+        console.log("Error while adding transaction in queue ::", error);
+    }
+};
+
+const transactionQueue = async (transactionId) => {
+    try {
+        const data = {
+            transactionId: transactionId,
+        };
+        const jobId = await redisClient.rpush(
+            "queue:t:transactions",
+            JSON.stringify(data)
+        );
+        return jobId;
+    } catch (error) {
+        console.log("Error while adding transaction in queue ::", error);
+    }
+};
+
+export { emailQueue, transactionQueue };
