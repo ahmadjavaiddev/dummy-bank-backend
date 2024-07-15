0b337aebde0a9c417f1a4e9b234b99ccac397b0f
ahmadjavaiddev <ahmadjavaiddev@gmail.com>
2024-07-15 10:23:30 +0500

    Add encrypt and decrypt function using "AES-256" algorithm to securely store MPIN in DB

--- src/utils/index.js
+++ src/utils/index.js
@@ -15,9 +15,49 @@ const generateVerificationToken = () => {
 };
 
 const verificationUrl = (req, token, type) => {
-    return `${req.protocol}://${req.get(
-        "host"
-    )}/api/v1/${type}/verify/${token}`;
+    if (!type) {
+        return `${req.protocol}://${req.get(
+            "host"
+        )}/api/v1/users/verify/${token}`;
+    }
+    if (type === "forgot") {
+        return `${req.protocol}://${req.get(
+            "host"
+        )}/api/v1/users/reset-password/${token}`;
+    }
+    if (type === "transactions") {
+        return `${req.protocol}://${req.get(
+            "host"
+        )}/api/v1/transactions/verify/${token}`;
+    }
 };
 
-export { generateVerificationToken, verificationUrl };
+const encryptMPIN = (mpin) => {
+    const key = crypto.randomBytes(32); // AES-256 requires a 32-byte key
+    const iv = crypto.randomBytes(16); // AES requires a 16-byte IV
+
+    const cipher = crypto.createCipheriv("aes-256-cbc", key, iv);
+    let encrypted = cipher.update(mpin, "utf8", "hex");
+    encrypted += cipher.final("hex");
+    return { encrypted, key, iv };
+};
+
+const decryptMPIN = (encryptedMPIN, key, iv) => {
+    const decipher = crypto.createDecipheriv("aes-256-cbc", key, iv);
+    let decrypted = decipher.update(encryptedMPIN, "hex", "utf8");
+    decrypted += decipher.final("utf8");
+    return decrypted;
+};
+
+const cryptoTokenVerify = (token) => {
+    let hashedToken = crypto.createHash("sha256").update(token).digest("hex");
+    return hashedToken;
+};
+
+export {
+    generateVerificationToken,
+    verificationUrl,
+    encryptMPIN,
+    decryptMPIN,
+    cryptoTokenVerify,
+};
