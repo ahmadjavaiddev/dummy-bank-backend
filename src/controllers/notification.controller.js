49413a891ec3f455d9c6805ef691e645419b4d92
ahmadjavaiddev <ahmadjavaiddev@gmail.com>
2024-07-27 23:26:41 +0500

    Remove createNotification controller and route

--- src/controllers/notification.controller.js
+++ src/controllers/notification.controller.js
@@ -3,53 +3,6 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { ApiError } from "../utils/ApiError.js";
 import Notification from "../models/notification.model.js";
-import { SocketMapUsers } from "../socket/index.js";
-
-const createNotification = asyncHandler(async (req, res) => {
-    const io = req.app.get("io");
-    const notifications = req.body; // Assuming req.body is an array of notifications
-
-    // Validate each notification and create them
-    const createdNotifications = await Promise.all(
-        notifications.map(async (notificationData) => {
-            const { userId, from, to, type, status, message } =
-                notificationData;
-
-            const userVerified = await User.findOne({
-                _id: userId,
-                isEmailVerified: true,
-            });
-            if (!userVerified) {
-                throw new ApiError(400, `User ${userId} is not verified`);
-            }
-
-            // Create Notification
-            const notification = await Notification.create({
-                userId: userId,
-                from: from,
-                to: to,
-                type: type,
-                status: status,
-                message: message,
-            });
-
-            if (!notification) {
-                throw new ApiError(400, "Notification not created");
-            }
-
-            return notification;
-        })
-    );
-
-    // Emit notifications via Socket.IO
-    createdNotifications.forEach((notification) => {
-        const userId = SocketMapUsers[notification.userId];
-        console.log("Notification :: userId ::", userId);
-        io.to(userId).emit("notifications", notification);
-    });
-
-    return res.status(201).json(new ApiResponse(201, null, "Success"));
-});
 
 const getNotifications = asyncHandler(async (req, res) => {
     const userId = req.user._id;
@@ -68,4 +21,4 @@ const getNotifications = asyncHandler(async (req, res) => {
     return res.status(201).json(new ApiResponse(201, sorted, "Success"));
 });
 
-export { createNotification, getNotifications };
+export { getNotifications };
--- src/routes/notification.route.js
+++ src/routes/notification.route.js
@@ -1,13 +1,9 @@
 import express from "express";
 import verifyJWT from "../middlewares/auth.middleware.js";
-import {
-    createNotification,
-    getNotifications,
-} from "../controllers/notification.controller.js";
+import { getNotifications } from "../controllers/notification.controller.js";
 
 const router = express.Router();
 
-router.post("/create", createNotification);
 router.get("/get", verifyJWT, getNotifications);
 
 export default router;
